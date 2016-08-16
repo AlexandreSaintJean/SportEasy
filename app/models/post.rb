48ade539be344df91042420e1f08110e1b3705ab
AlexandreSaintJean <xelastjean@gmail.com>
2016-08-16 15:49:32 +0200

    change from See.db to rails console

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
-  Validates :question, presence: true
+  validates :question, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,6 @@ class Product < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :address, presence: true
+  validates :city, presence: true
   validates :price_per_day, presence: true
 end

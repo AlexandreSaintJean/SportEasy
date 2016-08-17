886719ecfaa4b64c8f280b6f8614d8622275cbb7
AlexandreSaintJean <xelastjean@gmail.com>
2016-08-17 10:20:16 +0200

    last change for Jquery Form

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,9 +2,19 @@
 // All this logic will automatically be available in application.js.
 
 $(function() {
+    $(".profil-actions").hover(function() {
+      $(this).css('cursor','pointer');
+  });
 
-$( ".profil-actions" ).click(function() {
-  $(".current-profil").addClass( "hidden" );
-  $(".update-form").removeClass( "hidden" );
+  $( ".profil-actions" ).click(function() {
+    $(".current-profil").addClass( "hidden" );
+    $(".update-form").removeClass( "hidden" );
+  });
 });
 
+
+
+
+// $( ".profil-actions" ).click(function() {
+//   $(".current-profil").addClass( "hidden" );
+//   $(".update-form").removeClass( "hidden" );
